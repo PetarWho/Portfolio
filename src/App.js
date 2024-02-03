0141f17a3d364c567ae7d4a63cf154c8c84e0733
Roy <92612927+PetarWho@users.noreply.github.com>
2024-02-04 00:20:19 +0200

    React Update

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ import Contacts from "./layouts/contacts/Contacts";
 function App() {
   return (
     <div className="App flex flex-col min-h-screen">
-      <BrowserRouter>
+      <BrowserRouter basename='/Portfolio'>
         <div className="pt-16 overflow-y-auto flex-grow">
           <Header />
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route exact path="/Portfolio" element={<Home />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/certificates" element={<Certificates />} />
             <Route path="/about" element={<About />} />
