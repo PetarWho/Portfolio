a93acfd2f2d299f1a4a2ad1481f1865a44839147
Roy <92612927+PetarWho@users.noreply.github.com>
2024-02-04 04:32:11 +0200

    Updated MobileMenu

--- src/layouts/header/MobileMenu.jsx
+++ src/layouts/header/MobileMenu.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Linknk } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function MobileMenu() {
     const [isOpen, setIsOpen] = useState(false);
