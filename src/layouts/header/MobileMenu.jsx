7b3419107fb8c2de09ca709c5d04e133cd942a1e
Roy <92612927+PetarWho@users.noreply.github.com>
2024-02-04 04:28:25 +0200

    Updated MobileMenu

--- src/layouts/header/MobileMenu.jsx
+++ src/layouts/header/MobileMenu.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Linknk } from "react-router-dom";
 
 function MobileMenu() {
     const [isOpen, setIsOpen] = useState(false);
@@ -9,43 +10,33 @@ function MobileMenu() {
 
     return (
         <div className="relative">
-            <button className="block md:hidden text-white focus:outline-none mr-10" onClick={toggleMenu}>
+            <button className="block md:hidden text-white focus:outLinkne-none mr-10" onCLinkck={toggleMenu}>
                 <i className="fas fa-bars text-3xl"></i>
             </button>
             <div className={`bg-gray-900 absolute top-full right-0 mobile-menu rounded-md shadow-lg overflow-hidden transition-all duration-300 ease-in-out ${isOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'}`} style={{ minWidth: '150px' }}>
                 {isOpen && (
                     <div className="py-2">
                         <ul className="text-white text-center">
-                            <li className="py-2">
-                                <a className="block px-4 hover:bg-gray-800" href="/">
-                                    HOME
-                                </a>
-                            </li>
-                            <li className="py-2">
-                                <a className="block px-4 hover:bg-gray-800" href="/projects">
+                            <Link className="py-2">
+                                <a className="block px-4 hover:bg-gray-800" to="projects">
                                     PROJECTS
                                 </a>
-                            </li>
-                            <li className="py-2">
-                                <a className="block px-4 hover:bg-gray-800" href="/certificates">
+                            </Link>
+                            <Link className="py-2">
+                                <a className="block px-4 hover:bg-gray-800" to="certificates">
                                     CERTIFICATES
                                 </a>
-                            </li>
-                            <li className="py-2">
-                                <a className="block px-4 hover:bg-gray-800" href="/stories">
-                                    STORIES
-                                </a>
-                            </li>
-                            <li className="py-2">
-                                <a className="block px-4 hover:bg-gray-800" href="/about">
+                            </Link>
+                            <Link className="py-2">
+                                <a className="block px-4 hover:bg-gray-800" to="about">
                                     ABOUT
                                 </a>
-                            </li>
-                            <li className="py-2">
-                                <a className="block px-4 hover:bg-gray-800" href="/contacts">
+                            </Link>
+                            <Link className="py-2">
+                                <a className="block px-4 hover:bg-gray-800" to="contacts">
                                     CONTACTS
                                 </a>
-                            </li>
+                            </Link>
                         </ul>
                     </div>
                 )}
